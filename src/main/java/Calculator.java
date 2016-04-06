9d752e60b973d598cb4e80fd7ea17dbf0a668d0f
Alicia_Hellberg <alicia@romram.nu>
2016-04-06 15:15:36 +0200

    Created 2 more methods, subtraction and divide

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,19 @@
 
 public class Calculator {
+	double result;
 	
 	public double addition(double firstValue, double secondValue){
-		double result;
+		
 		result = firstValue + secondValue;
 		return result;
 	}
+	public double subtraction(double firstValue, double secondValue){
+		result = firstValue - secondValue;
+		return result;
+	}
+	public double divide(double firstValue, double secondValue){
+		result = firstValue - secondValue;
+		return result;
+	}
+	
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double firstNumber=3.5;
+		double secondNumber = 6.5;
 		Calculator calculator = new Calculator();
-		System.out.println(calculator.addition(3, 4));
+		System.out.println(calculator.addition(firstNumber,secondNumber));
+		System.out.println(calculator.subtraction(firstNumber,secondNumber));
+		System.out.println(calculator.divide(firstNumber,secondNumber));
 	}
 
 }
