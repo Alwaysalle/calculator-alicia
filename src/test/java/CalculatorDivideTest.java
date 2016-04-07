88866c9eaebd07f75be07451d7273a9cc6ea824c
Alicia <Praktikant@qa-retinas-MacBook-Pro.local>
2016-04-07 12:48:13 +0200

    Completed the remaining @test for divide, multiplication methods

--- src/test/java/CalculatorDivideTest.java
+++ src/test/java/CalculatorDivideTest.java
@@ -4,14 +4,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorDivideTest {
+	Calculator calculator;
+	double possitiveNr = 45.5;
+	double negativeNr = -7.3;
 
 	@Before
 	public void setUp() throws Exception {
+		calculator = new Calculator();
+	}
+	@Test
+	public void testPossitive() {
+		assertEquals(1,calculator.divide(possitiveNr,possitiveNr), 0.1d);
+	}
+	@Test
+	public void testNegative() {
+		assertEquals(1,calculator.divide(negativeNr,negativeNr), 0.1d);
+	}
+	@Test
+	public void testNegativePossitive() {
+		assertEquals(-0.16,calculator.divide(negativeNr,possitiveNr), 0.1d);
+	}
+	@Test
+	public void testPossitiveNegative() {
+		assertEquals(-6.23,calculator.divide(possitiveNr,negativeNr), 0.1d);
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
--- src/test/java/CalculatorMultiplicationTest.java
+++ src/test/java/CalculatorMultiplicationTest.java
@@ -4,14 +4,28 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorMultiplicationTest {
+	Calculator calculator;
+	double possitiveNr = 1.5;
+	double negativeNr = -3.3;
 
 	@Before
 	public void setUp() throws Exception {
+		calculator = new Calculator();
 	}
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPossitive() {
+		assertEquals(2.25,calculator.multiplication(possitiveNr,possitiveNr), 0.1d);
 	}
-
+	@Test
+	public void testNegative() {
+		assertEquals(10.8,calculator.multiplication(negativeNr,negativeNr), 0.1d);
+	}
+	@Test
+	public void testNegativePossitive() {
+		assertEquals(-5,calculator.multiplication(negativeNr,possitiveNr), 0.1d);
+	}
+	@Test
+	public void testPossitiveNegative() {
+		assertEquals(-5,calculator.multiplication(possitiveNr,negativeNr), 0.1d);
+	}
 }
